0ab67bd2c7761cae7f67f3d8fca1a2ea49467a24
dadezeeuw <dadezeeuw@psara.com>
2026-09-12 22:02:42 -0400

    supabase url and key

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Public browser configuration. Never paste a secret/service_role key here.
 window.MEAL_CONFIG = Object.freeze({
-  SUPABASE_URL: '',
-  SUPABASE_PUBLISHABLE_KEY: ''
+  SUPABASE_URL: 'https://kpprcluafhgauyhubhti.supabase.co/rest/v1/',
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_dKJWbItiUE8E6PVT6R7fBQ_8M0nq54t'
 });
